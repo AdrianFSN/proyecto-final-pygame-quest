a00a32070884adf5db8fb1258a9feac88498dceb
Adrián S <adriansotes@gmail.com>
2023-10-31 14:00:05 +0100

    feat: creo el db manager de la base de datos

---
+++ glumtar/data/db_manager.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+"""Mi primera consulta para leer datos es SELECT Position, Name, Score FROM Glumtar_best_scores;"""
+
+
+class DBManager:
+    def __init__(self, db_route):
+        self.db_route = db_route
+
+    def consultSQL(self, consult):
+        # 1. Conectar a la base de datos
+        connection = sqlite3.connect(self.db_route)
+
+        # 2. Abrir cursor
+        cursor = connection.cursor()
+
+        # 3. Ejecutar la consulta
+        cursor.execute(consult)
+
+        # 4. Tratar los datos
+        # 4.1 Obtener los datos
+        # Da una lista. Hay un fetchone también.
+        returned_data = cursor.fetchall()
+
+        # 4.2 Los guardo localmente
+        self.recorded_best_scores = []
+        column_names = []
+        # Esto me va a dar una tupla con los nombres de la columna de la base de datos.
+        for column in cursor.description:
+            column_names.append(column[0])
+
+        for data_in_recorded_row in returned_data:  # Esto me va a venir en una tupla con todos los datos del record
+            # Diccionario vacío que tendrá los nombres de las columnas como key y los datos de cada columna como value.
+            record = {}
+            value = 0
+            for name in column_names:
+                record[name] = data_in_recorded_row[value]
+                value += 1
+                self.recorded_best_scores.append(record)
+
+        # 5. Cerrar la conexión
+        connection.close()
+
+        # 6. Devolver los resultados
+        return self.recorded_best_scores
